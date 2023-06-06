23d3d7816bb326858f9bf5ce7c9ca630aee6dc12
tamerlansarakaev <timasarakaev@gmail.com>
2023-06-06 23:45:36 +0300

    [develop/watch-page] Add some dependencies and add basic information for watch page, fix some bugs.

--- src/components/AnimeCard/AnimeCard.tsx
+++ src/components/AnimeCard/AnimeCard.tsx
@@ -1,22 +1,25 @@
+import { NavLink } from "react-router-dom";
 import { IAnime } from "../../types";
 import { checkGenres } from "../../utils";
 
 import styles from "./AnimeCard.module.scss";
 
-const AnimeCard = ({ name, genres, itemImage }: IAnime) => {
+const AnimeCard = ({ name, genres, backgroundImage, code }: IAnime) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <img src={itemImage} className={styles.imageContainer__image} />
+    <NavLink to={`/anime/${code}`} className={styles.navigate}>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img src={backgroundImage} className={styles.imageContainer__image} />
+        </div>
+        <div className={styles.info}>
+          <span className={styles.info__title}>{name}</span>
+          <span className={styles.info__genresTitle}>
+            Жанры:{" "}
+            <span className={styles.info__genres}>{checkGenres(genres)}</span>
+          </span>
+        </div>
       </div>
-      <div className={styles.info}>
-        <span className={styles.info__title}>{name}</span>
-        <span className={styles.info__genresTitle}>
-          Жанры:{" "}
-          <span className={styles.info__genres}>{checkGenres(genres)}</span>
-        </span>
-      </div>
-    </div>
+    </NavLink>
   );
 };
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,8 +4,14 @@ import { useAppDispatch, useAppSelector } from "../../redux/config";
 // Api
 import ApiService from "../../api/actions/index";
 
+// BG
+import BackgroundImage from "../../assets/images/background.webp";
+
 // Interface
 import { IAnime } from "../../types/index";
+
+// Styles
+import styles from "./HomePage.module.scss";
 
 import AnimeCardList from "../../components/AnimeCardList/AnimeCardList";
 import Main from "../../components/Main/Main";
@@ -25,13 +31,14 @@ const HomePage = () => {
 
   async function handleClickMore() {
     setStatus(true);
-    const newAnimeList = await ApiService.getAnimeFromPage(page + 1);
+    const newPage = page + 1;
+    const newAnimeList = await ApiService.getAnimeFromPage(newPage);
 
     if (newAnimeList) {
       dispatch(
         loadAnime({
           animeList: [...animeList, ...newAnimeList] as IAnime[],
-          page,
+          page: newPage,
         })
       );
     }
@@ -40,35 +47,38 @@ const HomePage = () => {
   }
 
   return (
-    <Main title="СПИСОК АНИМЕ">
-      <AnimeCardList animeList={animeList} limit={limit} />
-      {limit <= animeList.length && (
-        <Button
-          onClick={handleClickMore}
-          disabled={status}
-          sx={{
-            width: "fit-content",
-            margin: "0 auto",
-            borderRadius: "12px",
-            background: "var(--standartRedColor)",
-            padding: "10px 22px",
-            fontWeight: 700,
-            fontSize: "18px",
-            lineHeight: "21px",
-            textTransform: "uppercase",
-            color: "white",
-            ":hover": {
-              background: "var(--hoverRedColor)",
-            },
-            ":disabled": {
+    <>
+      <img src={BackgroundImage} className="bg" />
+      <div className={styles.container}>
+        <Main title="СПИСОК АНИМЕ">
+          <AnimeCardList animeList={animeList} limit={limit} />
+          <Button
+            onClick={handleClickMore}
+            disabled={status}
+            sx={{
+              width: "fit-content",
+              margin: "0 auto",
+              borderRadius: "12px",
+              background: "var(--standartRedColor)",
+              padding: "10px 22px",
+              fontWeight: 700,
+              fontSize: "18px",
+              lineHeight: "21px",
+              textTransform: "uppercase",
               color: "white",
-            },
-          }}
-        >
-          {status ? "Загрузка..." : "Показать больше"}
-        </Button>
-      )}
-    </Main>
+              ":hover": {
+                background: "var(--hoverRedColor)",
+              },
+              ":disabled": {
+                color: "white",
+              },
+            }}
+          >
+            {status ? "Загрузка..." : "Показать больше"}
+          </Button>
+        </Main>
+      </div>
+    </>
   );
 };
 
