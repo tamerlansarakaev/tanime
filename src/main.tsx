0407a2c842b0abe287f696ecce4544a15750440a
tamerlansarakaev <timasarakaev@gmail.com>
2023-06-26 17:12:49 +0300

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/config/store.ts";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
